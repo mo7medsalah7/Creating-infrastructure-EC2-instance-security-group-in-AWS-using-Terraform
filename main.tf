00c3d94a2c6c9593130a59305f479118f9e9c690
mo7medsalah7 <msmido9@gmail.com>
2023-03-03 14:52:42 +0200

    assign elastic ip

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
-resource "aws_instance" "my-ec2"{
-    ami="ami-0f3c9c466bb525749"
-    instance_type="t2.micro"
-    tags = {
-        Name = "mosalah-ec2-instance"
-    }
+resource "aws_instance" "my-ec2" {
+  ami           = "ami-0f3c9c466bb525749"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "mosalah-ec2-instance"
+  }
+}
+
+resource "aws_eip" "my-eip" {
+  vpc = true
+}
+
+
+resource "aws_eip_association" "associate" {
+  instance_id   = aws_instance.my-ec2.id
+  allocation_id = aws_eip.my-eip.id
+
 }
